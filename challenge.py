c2c8a61fe7b06e33c33fd75f88df181c070c5425
Ruth Wangare AideenHah-yil <wangarenjorogeruth@gmail.com>
2024-03-12 05:25:35 -0700

    initial commit

---
+++ challenge.py
@@ -0,0 +1,28 @@
+def large_power(base, exponent):
+    result = base ** exponent
+
+    if result > 5000:
+        return True
+    else:
+        return False
+
+# Calling the large_power function and printing the results
+    print("Script started executing...")
+    print(large_power(10, 3))
+    print(large_power(2, 10))
+    print("Script finished executing.")
+
+# Challenge 2
+def divisible_by_ten(num):
+    result = num % 10
+    if result == 0:
+        return True
+    else:
+        return False
+
+# Calling the divisible_by_ten function and printing the results
+print("Script started executing...")
+print(divisible_by_ten(10))
+print(divisible_by_ten(20))
+print(divisible_by_ten(30))
+print("Script finished executing.")
